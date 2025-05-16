50f98727c6fa041a023c58c50211a162bfd7c915
Collin Sparks <cklsparks@gmail.com>
2025-05-16 13:12:33 -0400

    added draft Applicant model

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Applicant(models.Model):
+    firstname = models.Charfield(max_length=200)
+    lastname = models.Charfield(max_length=200)
+    email = models.EmailField(blank=True) # allow users w no email
+    phone = models.Charfield(max_length=200)
+    org_affiliation = models.Charfield(max_length=200)
+    station_preference = models.JSONField() # e.g. {"games": False, "ticket_sales": True, ... } bc no array model field in SQLite
